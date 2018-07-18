18fc6202f4b39b6c593bacbb3b91496f0567f3a8
MCampbell08 <WaffleDefend29@gmail.com>
2018-07-18 00:33:21 -0600

    Added In Adding new word to Stack

--- Lab05_ContextFreeLanguage/Lab05_ContextFreeLanguage/ConsoleManager.cs
+++ Lab05_ContextFreeLanguage/Lab05_ContextFreeLanguage/ConsoleManager.cs
@@ -7,16 +7,16 @@ namespace Lab05_ContextFreeLanguage
     public class ConsoleManager
     {
         private static Stack<Object> stack = new Stack<object>();
-        private string[] acceptableArticles = new string[] { "a", "the", "an" };
-        private string[] acceptableNouns = new string[] { "dog", "cat", "squirrel" };
-        private string[] acceptableVerbs = new string[] { "ran", "bites", "scurries", "sat" };
-        private string[] acceptablePronouns = new string[] { "he", "she", "him", "her" };
-        private string[] acceptableAdjectives = new string[] { "happily", "quickly", "good", "bad" };
-        private string[] acceptablePrepostions = new string[] { "with", "from", "to" };
-        private string[] acceptableVerbPhrases = new string[] { "Verb", "Verb Adjective", "Verb NounPhrase", "Verb Adjective NounPhrase", "Verb Adjective ProunounPhrase" };
-        private string[] acceptableNounPhrases = new string[] { "Article Noun", "Article Adjective Noun", "Article Noun Preposition NounPhrase", "NounPhrase Preposition NounPhrase" };
-        private string[] acceptablePronounPhrases = new string[] { "Pronoun", "Preposition Pronoun" };
-        private string[] acceptableSentences = new string[] { "NounPhrase VerbPhrase", "PronounPhrase VerbPhrase" };
+        private List<string> acceptableArticles = new List<string> { "a", "the", "an" };
+        private List<string> acceptableNouns = new List<string> { "dog", "cat", "squirrel" };
+        private List<string> acceptableVerbs = new List<string> { "ran", "bites", "scurries", "sat" };
+        private List<string> acceptablePronouns = new List<string> { "he", "she", "him", "her" };
+        private List<string> acceptableAdjectives = new List<string> { "happily", "quickly", "good", "bad" };
+        private List<string> acceptablePrepostions = new List<string> { "with", "from", "to" };
+        private List<string> acceptableVerbPhrases = new List<string> { "Verb", "Verb Adjective", "Verb NounPhrase", "Verb Adjective NounPhrase", "Verb Adjective ProunounPhrase" };
+        private List<string> acceptableNounPhrases = new List<string> { "Article Noun", "Article Adjective Noun", "Article Noun Preposition NounPhrase", "NounPhrase Preposition NounPhrase" };
+        private List<string> acceptablePronounPhrases = new List<string> { "Pronoun", "Preposition Pronoun" };
+        private List<string> acceptableSentences = new List<string> { "NounPhrase VerbPhrase", "PronounPhrase VerbPhrase" };
 
         private string[] inputStream;
         private TreeSystem system = new TreeSystem();
@@ -25,11 +25,68 @@ namespace Lab05_ContextFreeLanguage
         {
             while((inputStream = Console.ReadLine().Split(' ')) != new string[] { "quit" })
             {
+                foreach (string word in inputStream)
+                {
+                    if (CanAddToStack(word))
+                    {
 
+                    }
+                    else
+                    {
+                        PrintMessage();
+                    }
+                }
             }
             
         }
 
-        private void 
+        private bool CanAddToStack(string input)
+        {
+            if (acceptableArticles.Contains(input))
+            {
+                stack.Push(input);
+            }
+            else if (acceptableNouns.Contains(input))
+            {
+                stack.Push(input);
+            }
+            else if (acceptableVerbs.Contains(input))
+            {
+                stack.Push(input);
+            }
+            else if (acceptablePronouns.Contains(input))
+            {
+                stack.Push(input);
+            }
+            else if (acceptableAdjectives.Contains(input))
+            {
+                stack.Push(input);
+            }
+            else if (acceptablePrepostions.Contains(input))
+            {
+                stack.Push(input);
+            }
+            else if (acceptableVerbPhrases.Contains(input))
+            {
+                stack.Push(input);
+            }
+            else if (acceptableNounPhrases.Contains(input))
+            {
+                stack.Push(input);
+            }
+            else if (acceptablePronounPhrases.Contains(input))
+            {
+                stack.Push(input);
+            }
+            else if (acceptableSentences.Contains(input))
+            {
+                stack.Push(input);
+            }
+            else
+            {
+                return false;
+            }
+            return true;
+        }
     }
 }
